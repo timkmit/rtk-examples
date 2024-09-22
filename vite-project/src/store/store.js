5a8c51f0f5b0ad1a0c18408f5d04ef791d168d95
Timur <75565352+timkmit@users.noreply.github.com>
2024-09-22 19:10:27 +0300

    Delete vite-project directory

--- vite-project/src/store/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/CounterSlice';
-const store = configureStore({
-  reducer: { counter: counterReducer },
-});
-export default store;
